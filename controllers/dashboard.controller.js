d9dde0c5910c41fc6c0088de92b592ba94fd9f53
Le Minh <le.minh011100@gmail.com>
2022-09-25 12:50:38 +0700

    added dashboard welcome

---
+++ controllers/dashboard.controller.js
@@ -0,0 +1,17 @@
+class DashboardController {
+  dashboard(req, res) {
+    res.setHeader("Content-Type", "text/html")
+    res.send(
+      `
+      <center>
+        <div>This is api Marvel Cinematic Universe</div>
+        <div>/categories</div>
+        <div>/movies</div>
+        <div>/characters</div>
+      </center>
+    `
+    );
+  }
+}
+
+module.exports = new DashboardController();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const dashboard = require('./dashboard');
 const moviesRoute = require('./movies');
 const categoriesRoute = require('./categories');
 
 function directionRoute(app) {
+  app.use('/', dashboard);
   app.use('/movies', moviesRoute);
   app.use('/categories', categoriesRoute);
 }
